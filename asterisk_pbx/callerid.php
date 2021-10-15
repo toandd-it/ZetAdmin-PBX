593e1016388b6afc0726f3ca5836c7ebc48b12d2
Toan IT <toandd.it@gmail.com>
2021-10-15 11:39:16 +0700

    update

--- asterisk_pbx/callerid.php
+++ asterisk_pbx/callerid.php
@@ -7,6 +7,37 @@ include('phpagi.php');
 $agi = new AGI();
 $data = $agi->get_variable();
 
+
+//Start debug
+$debugFile = '/var/lib/asterisk/agi-bin/debug.txt';
+$msgDebug = "";
+foreach($data as $name => $value)
+{
+    $msgDebug .= ucfirst($name).": ".$value." | ";
+}
+$maxSize = 10485760; //10M
+if(file_exists($debugFile))
+{
+    $fileSize = filesize($debugFile);
+    if($fileSize >= $maxSize)
+    {
+        if(rename($debugFile, '/var/lib/asterisk/agi-bin/debug-'.date('Y_m_d_H_i_s', filemtime($debugFile)).'-to-'.date('Y_m_d_H_i_s', time()).'.txt'))
+        {
+			file_put_contents($debugFile, $msgDebug, FILE_APPEND | LOCK_EX);
+        }
+    }
+    else
+    {
+        file_put_contents($debugFile, "\n".$msgDebug, FILE_APPEND | LOCK_EX);
+    }
+}
+else
+{
+    file_put_contents($debugFile, $msgDebug, FILE_APPEND | LOCK_EX);
+}
+//End debug
+
+
 //get_variable
 //set_variable
 ?>
