4c83aed25ec833896481f8eaef7813c0c308d0d6
Toan IT <toandd.it@gmail.com>
2021-10-15 10:21:09 +0700

    update

--- asterisk_pbx/language/vn.php
+++ asterisk_pbx/language/vn.php
@@ -1,3 +1,38 @@
 <?php
 $_text['postback'] = '';
+
+$_text['msg_000'] = 'Địa chỉ IP không được cấp phép!';
+$_text['msg_001'] = 'Hành động không được cấp phép!';
+$_text['msg_002'] = 'API Key không đúng!';
+$_text['msg_003'] = 'AGI không thể kết nối';
+$_text['msg_004'] = 'Bị cấm, API KEY không được cấp phép!';
+$_text['msg_005'] = 'module reload thành công!';
+$_text['msg_006'] = 'sip reload thành công!';
+$_text['msg_007'] = 'queue unpause member SIP/%s queue %s thành công!';
+$_text['msg_008'] = 'queue pause member SIP/%s queue %s thành công!';
+$_text['msg_009'] = 'queue remove member SIP/%s queue %s thành công!';
+$_text['msg_010'] = 'queue add member SIP/%s queue %s thành công!';
+$_text['msg_011'] = 'Tài khoản SIP %s đang gọi %s.';
+$_text['msg_012'] = 'Số điện thoại không được bỏ trống!';
+$_text['msg_013'] = 'Không tồn tại dữ liệu trên hệ thống!';
+$_text['msg_014'] = 'Xóa lịch sử cuộc gọi %s thành công!';
+$_text['msg_015'] = 'Máy chủ đang bận vui lòng thử lại sau! %s';
+$_text['msg_016'] = 'Xóa thành công: %s. Lỗi: %s. Không tồn tại: %s. Tổng: %s';
+$_text['msg_017'] = 'Chưa chọn lược sử nào để xóa!';
+$_text['msg_018'] = 'Xóa kịch bản %s thành công!';
+$_text['msg_019'] = 'Cập nhật kịch bản %s thành công!';
+$_text['msg_020'] = 'Kịch bản %s đã tồn tại trên hệ thống!';
+$_text['msg_021'] = 'Thêm kịch bản %s thành công!';
+$_text['msg_022'] = 'Xóa hàng đợi %s thành công!';
+$_text['msg_023'] = 'Cập nhật hàng đợi %s thành công!';
+$_text['msg_024'] = 'Hàng đợi %s đã tồn tại trên hệ thống!';
+$_text['msg_025'] = 'Thêm mới hàng đợi %s thành công!';
+$_text['msg_026'] = 'Xóa SIP Trunk %s thành công!';
+$_text['msg_027'] = 'Cập nhật SIP Trunk %s thành công!';
+$_text['msg_028'] = 'Xóa tài khoản SIP %s thành công!';
+$_text['msg_029'] = 'Cập nhật tài khoản SIP %s thành công!';
+$_text['msg_030'] = 'Tài khoản SIP %s đã tồn tại trên hệ thống!';
+$_text['msg_031'] = 'Tạo tài khoản SIP %s thành công!';
+$_text['msg_032'] = 'SIP trunk %s đã tồn tại trên hệ thống!';
+$_text['msg_033'] = 'Thêm SIP trunk %s thành công!';
 ?>
