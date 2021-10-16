ac8cefd8c3b3a418c069b45d19422a0a1146e1dd
Toan IT <toandd.it@gmail.com>
2021-10-16 11:02:28 +0700

    update

--- install.sh
+++ install.sh
@@ -356,7 +356,7 @@ web_api_id=$(openssl rand -hex 16)
 web_api_key=$(openssl rand -hex 24)
 api_conf=api/asterisk_pbx/config.php
 sudo touch $webroot/$api_conf
-echo '<?php'
+echo '<?php' >> $webroot/$api_conf
 echo '$api_url = "https://'$pbx_domain'/api/asterisk_pbx/postback.php";' >> $webroot/$api_conf
 echo '$api_id = "'$web_api_id'";' >> $webroot/$api_conf
 echo '$api_key = "'$web_api_key'";' >> $webroot/$api_conf
