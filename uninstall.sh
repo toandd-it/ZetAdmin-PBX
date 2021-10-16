87ab13157f3d5a6bf416f98ea638e23c7687e24a
Toan IT <toandd.it@gmail.com>
2021-10-16 09:36:57 +0700

    update

--- uninstall.sh
+++ uninstall.sh
@@ -21,6 +21,7 @@ sudo rm -rf /usr/lib/systemd/system/pbxlog.service
 sudo rm -rf /etc/fail2ban/jail.d/asterisk.local
 
 sudo systemctl stop lsws.service
+sudo systemctl disable lsws.service
 sudo rm -rf /usr/local/lsws/
 sudo rm -rf /var/www/public_html/
 echo -e "\033[32mRemove OpenLiteSpeed successful!\033[m"
@@ -28,6 +29,7 @@ echo -e "\033[32mRemove OpenLiteSpeed successful!\033[m"
 sleep 0.5
 
 sudo systemctl stop mongod.service
+sudo systemctl disable mongod.service
 sudo yum -y erase $(rpm -qa | grep mongodb-org)
 sudo rm -r /var/log/mongodb
 sudo rm -r /var/lib/mongo
@@ -35,6 +37,7 @@ echo -e "\033[32mRemove MongoDB successful!\033[m"
 sleep 0.5
 
 sudo systemctl stop asterisk
+sudo systemctl disable asterisk
 sudo rm -rf /etc/asterisk
 sudo rm -rf /var/log/asterisk
 sudo rm -rf /var/lib/asterisk
