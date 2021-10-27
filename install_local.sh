6bbddc758c5fdda43ebc80be5cdaf377e5eeb050
Toan IT <toandd.it@gmail.com>
2021-10-27 23:20:10 +0700

    update

--- install_local.sh
+++ install_local.sh
@@ -356,7 +356,7 @@ sudo touch $asterisk_etc/extensions_api.conf
 echo ';extensions_api.conf' >> $asterisk_etc/extensions_api.conf
 echo '[default_za]' >> $asterisk_etc/extensions.conf
 echo 'exten => _X.,1,Answer()' >> $asterisk_etc/extensions.conf
-echo '	same => n,Dial(SIP/${EXTEN})' >> $asterisk_etc/extensions.conf
+echo '	same => n,Dial(SIP/${EXTEN},30)' >> $asterisk_etc/extensions.conf
 echo '	same => n,Hangup()' >> $asterisk_etc/extensions.conf
 echo '' >> $asterisk_etc/extensions.conf
 echo '#include extensions_api.conf' >> $asterisk_etc/extensions.conf
