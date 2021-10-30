554bb872e8b8382c26dd409101a2dbc2dfed9d5d
Toan IT <toandd.it@gmail.com>
2021-10-30 17:23:24 +0700

    update

---
+++ asterisk_pbx/chan_dongle_install.sh
@@ -0,0 +1,14 @@
+
+
+wget http://mirror.centos.org/centos/7/os/x86_64/Packages/usb_modeswitch-2.5.1-1.el7.x86_64.rpm
+yum install -y usb_modeswitch-2.5.1-1.el7.x86_64.rpm
+
+wget https://github.com/oleg-krv/asterisk-chan-dongle/archive/asterisk13.zip
+unzip asterisk13.zip
+cd asterisk13
+aclocal && autoconf && automake -a
+./configure --with-astversion=13.13.1
+make 
+make install
+cp chan_dongle.so /usr/lib/asterisk/modules/
+cp etc/dongle.conf /etc/asterisk
