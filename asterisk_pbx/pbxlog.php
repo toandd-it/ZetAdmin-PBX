994361b0bb38e62b0e471212d392a0d656a8b1ff
Toan IT <toandd.it@gmail.com>
2021-11-13 11:53:02 +0700

    update

--- asterisk_pbx/pbxlog.php
+++ asterisk_pbx/pbxlog.php
@@ -67,88 +67,55 @@ else
 				/*call Event*/
 				if($res['Event'] == 'DialBegin')
 				{
-					$_mgid = (string)new \MongoDB\BSON\ObjectID;
 					$dialData = explode("/", $res['DialString']);
 					$Exten = empty($dialData[1]) ? $dialData[0] : $dialData[1];
-					$Trunk = isset($dialData[1]) ? $dialData[0] : '';
-					$campaign_id = '';
-					if(!empty($res['DestConnectedLineNum']))
-					{
-						//$campaignData = $mgdb->select('call_campaigns', ['context' => (string)$res['DestConnectedLineNum']]);
-						//if(!empty($campaignData['data']['_id']))
-						//{
-							//$campaign_id = (string)$campaignData['data']['_id'];
-						//}
-					}
 					
-					$trunkData = $mgdb->select('call_sip_trunk', ['_id' => (string)$Trunk]);
-					if(isset($trunkData['data']['prefix']))
-					{
-						$Exten = ltrim($Exten, $trunkData['data']['prefix']);
-					}
-					
-					$t_create = microtime(true);
-					$dataInsert = array(
-						'_id' => (string)$_id, 
-						'CallerIDNum' => (string)isset($res['CallerIDName']) ? $res['CallerIDName'] : $res['DestCallerIDNum'],
-						'Exten' => (string)$Exten,
-						'Context' => (string)$res['DestContext'],
-						'ConnectedLineNum' => (string)$res['DestConnectedLineNum'],
-						'ConnectedLineName' => (string)$res['DestConnectedLineName'],
-						'DialString' => $res['DialString'],
-						'Trunk' => (string)$Trunk,
-						'Uniqueid' => (float)$_id,
-						'campaign_id' => $campaign_id,
-						't_create' => $t_create, 
-						't_call' => 0,
-						't_up' => 0, 
-						't_hangup' => 0
-					);
+					$dataInsert = $res;
+					$dataInsert['_id'] = (float)$_id;
+					$dataInsert['Caller'] = '';
+					$dataInsert['Called'] = (string)$Exten;
+					$dataInsert['t_create'] = $_id;
+					$dataInsert['t_call'] = 0;
+					$dataInsert['t_up'] = 0;
+					$dataInsert['t_hangup'] = 0;
+
 					//$app->cdrSave($dataInsert);
 					$mgdb->insert($db_collection, $dataInsert);
-					if(!empty($campaign_id))
-					{
-						//$mgdb->update('call_campaign_contacts', ['campaign' => (string)$campaign_id, 'phone' => (string)$Exten], ['$set' => ['t_create' => $t_create, 'Uniqueid' => (string)$_id, 'channel' => (string)$channel]], []);
-						
-						$mgdb->update('call_campaigns', ['_id' => (string)$campaign_id], ['$inc' => ['totalCalled' => 1]], []);
-						$resCache[$channel]['campaign'] = $campaign_id;
-
-					    $app->cdrSave($dataInsert);
-					}
 				}
 				elseif($res['Event'] == 'AgentCalled')
 				{
 					$t_create_sub = microtime(true);
-					$update = $mgdb->update($db_collection, ['_id' => $channel], ['$set' => ['t_create_sub' => $t_create_sub, 'DestChannel' => $res['DestChannel'], 'MemberName' => $res['MemberName']]], []);
+					$update = $mgdb->update($db_collection, ['_id' => (float)$_id], ['$set' => ['t_create_sub' => $t_create_sub, 'DestChannel' => $res['DestChannel'], 'MemberName' => $res['MemberName']]], []);
 					if($update['status'] == false)
 					{
 						$res['t_create_sub'] = microtime(true);
+						$res['Error'] = 'Update AgentCalled';
 						$app->cdrSave($res);
 					}
-					//$mgdb->update('call_campaign_contacts', ['Uniqueid' => (string)$_id], ['$set' => ['agent' => $res['MemberName']]], []);
 				}
 				elseif($res['Event'] == 'DialEnd')
 				{
 					$t_up = microtime(true);
-					$update = $mgdb->update($db_collection, ['_id' => (string)$_id], ['$set' => ['t_up' => $t_up, 'DialStatus' => $res['DialStatus']]], []);
+					$update = $mgdb->update($db_collection, ['_id' => (float)$_id], ['$set' => ['t_up' => $t_up, 'DialStatus' => $res['DialStatus']]], []);
 					if($update['status'] == false)
 					{
 						$res['t_up'] = microtime(true);
+						$res['Error'] = 'Update DialEnd';
 						$app->cdrSave($res);
 					}
 					else
 					{
-						$resCache[$channel]['t_up'] = $t_up;
+						$resCache[$_id]['t_up'] = $t_up;
 					}
-					//$mgdb->update('call_campaign_contacts', ['Uniqueid' => (string)$_id], ['$set' => ['t_up' => $t_up, 'DialStatus' => $res['DialStatus']]], []);
 				}
 				elseif($res['Event'] == 'AgentConnect')
 				{
 					$t_up_sub = microtime(true);
-					$update = $mgdb->update($db_collection, ['_id' => (string)$_id], ['$set' => ['t_up_sub' => $t_up_sub]], []);
+					$update = $mgdb->update($db_collection, ['_id' => (float)$_id], ['$set' => ['t_up_sub' => $t_up_sub]], []);
 					if($update['status'] == false)
 					{
 						$res['t_up_sub'] = microtime(true);
+						$res['Error'] = 'Update AgentConnect';
 						$app->cdrSave($res);
 					}
 					else
@@ -159,53 +126,46 @@ else
 				elseif($res['Event'] == 'Hangup')
 				{
 					$t_hangup = microtime(true);
-					if(!empty($resCache[$channel]['t_up']))
+					if(!empty($resCache[$_id]['t_up']))
 					{
 						if(!empty($resCache[$channel]['t_up_sub']))
 						{
-							$setHangup = ['$set' => ['t_hangup' => $t_hangup, 'Context' => $res['Context'], 't_call' => ($t_hangup - $resCache[$channel]['t_up']), 't_call_sub' => ($t_hangup - $resCache[$channel]['t_up_sub']), 'cause_txt' => $res['Cause-txt']]];
+							$setHangup = ['$set' => ['t_hangup' => $t_hangup, 't_call' => ($t_hangup - $resCache[$_id]['t_up']), 't_call_sub' => ($t_hangup - $resCache[$channel]['t_up_sub']), 'cause_txt' => $res['Cause-txt']]];
 						}
 						else
 						{
-							$setHangup = ['$set' => ['t_hangup' => $t_hangup, 'Context' => $res['Context'], 't_call' => ($t_hangup - $resCache[$channel]['t_up']), 'cause_txt' => $res['Cause-txt']]];
+							$setHangup = ['$set' => ['t_hangup' => $t_hangup, 't_call' => ($t_hangup - $resCache[$_id]['t_up']), 'cause_txt' => $res['Cause-txt']]];
 						}
 					}
 					else
 					{
-						$setHangup = ['$set' => ['t_hangup' => $t_hangup, 'Context' => $res['Context'], 'cause_txt' => $res['Cause-txt']]];
+						$setHangup = ['$set' => ['t_hangup' => $t_hangup, 'cause_txt' => $res['Cause-txt']]];
 					}
-					$update = $mgdb->update($db_collection, ['_id' => (string)$_id], $setHangup, []);
+					$update = $mgdb->update($db_collection, ['_id' => (float)$_id], $setHangup, []);
 					if($update['status'] == false)
 					{
 						$res['t_hangup'] = microtime(true);
+						$res['Error'] = 'Update Hangup';
 						$app->cdrSave($res);
 					}
-					//$mgdb->update('call_campaign_contacts', ['Uniqueid' => (string)$_id], $setHangup, []);
-					if(!empty($resCache[$channel]))
+					if(!empty($resCache[$_id]))
 					{
-						unset($resCache[$channel]);
+						unset($resCache[$_id]);
 					}
 				}
-				$app->cdrSave($res);
 			}
 			else
 			{
-				if(isset($res['Event']) && isset($res['Variable']) && $res['Event'] == 'VarSet' && $res['Variable'] == 'keypad')
+				if(isset($res['Event']) && isset($res['Variable']) && $res['Event'] == 'VarSet' && isset($res['Value']))
 				{
-					if(!isset($res['Value']))
+					$update = $mgdb->update($db_collection, ['_id' => (float)$_id], ['$set' => ['Variable' => (string)$res['Variable'], 'Value' => (string)$res['Value']]], []);
+					if($update['status'] == false)
 					{
-						$res['Value'] = 0;
+						$res['Error'] = 'Update Keypad';
+						$app->cdrSave($res);
 					}
-					$mgdb->update($db_collection, ['_id' => (string)$_id], ['$set' => ['keypad' => (string)$res['Value']]], []);
-					$mgdb->update('call_campaign_contacts', ['Uniqueid' => (string)$_id], ['$set' => ['keypad' => (string)$res['Value']]], []);
-					
-					$app->cdrSave($res);
 				}
-				//$app->cdrSave($res);
-				/*other Event*/
-				// echo "<pre>";
-				// var_dump($res);
-				// echo "</pre>";
+				/* $app->cdrSave($res); */
 			}
 			$res = array();
 		}
