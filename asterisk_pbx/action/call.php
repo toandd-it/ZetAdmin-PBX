8de514f86383c868bb5ed2ad76d6960fc1b2bf2b
Toan IT <toandd.it@gmail.com>
2021-11-22 21:16:02 +0700

    update

--- asterisk_pbx/action/call.php
+++ asterisk_pbx/action/call.php
@@ -207,68 +207,23 @@ if(isset($action) && isset($mgdb))
                 $status = false;
                 $msg = $app->_lang('msg_013');
             }
-
-            $ext = $_POST['exten'];
-            $ext = filter_var($ext, FILTER_SANITIZE_NUMBER_INT);
-            $ext = preg_replace("/[^0-9,.]/", "", $ext);
-
-            $strChannel = "SIP/".$ext;
-            $strContext = isset($_POST['context']) ? $_POST['context'] : "az_default";
-            #specify the amount of time you want to try calling the specified channel before hangin up
-            $strWaitTime = 30;
-            #specify the priority you wish to place on making this call
-            $strPriority = 1;
-            #specify the maximum amount of retries
-            $strMaxRetry = 2;
-            
-            $number = strtolower($_POST['number']);
-            $number = filter_var($number, FILTER_SANITIZE_NUMBER_INT);
-            $number = preg_replace("/[^0-9,.]/", "", $number);
-            
-            $callerid = isset($_POST['number_name']) ? $_POST['number_name'] : "Click to dial";
-            $strCallerId = $callerid." <$number>";
-            
-            $oSocket = fsockopen("127.0.0.1", 5038, $errno, $errstr, 20);
-            if (!$oSocket) 
-            {
-                $status = false;
-                $msg = "$errstr ($errno)";
-            } 
-            else 
-            {
-                fputs($oSocket, "Action: login\r\n");
-                fputs($oSocket, "Events: off\r\n");
-                fputs($oSocket, "Username: $strUser\r\n");
-                fputs($oSocket, "Secret: $strSecret\r\n\r\n");
-                fputs($oSocket, "Action: originate\r\n");
-                fputs($oSocket, "Channel: $strChannel\r\n");
-                fputs($oSocket, "WaitTime: $strWaitTime\r\n");
-                fputs($oSocket, "CallerId: $strCallerId\r\n");
-                fputs($oSocket, "Exten: $number\r\n");
-                fputs($oSocket, "Context: $strContext\r\n");
-                fputs($oSocket, "Priority: $strPriority\r\n\r\n");
-                fputs($oSocket, "Action: Logoff\r\n\r\n");
-                sleep(2);
-                fclose($oSocket);
-                $status = true;
-                $msg = sprintf($app->_lang('msg_011'), $strChannel, $number);
-            }
         }
         else
         {
+            $campaign_id = '';
             $status = false;
-			$msg = $app->_lang('msg_012');
+            $msg = 'ID Chiến dịch không được bỏ trống!';
         }
         $insertLogs = array(
-			'id' => (array)$api_id,
+			'id' => (array)$campaign_id,
 			'tcreate' => time(),
 			'account' => '',
 			'action' => $action,
-			'email' => '',
+			'email' => $authorAdmin['email'],
 			'ip' => $ip,
 			'uag' => $uag,
 			'detail' => $msg,
-			'uid' => $api_id,
+			'uid' => $authorAdmin['id'],
 			'module' => '',
 			'route' => '',
 			'status' => $status
